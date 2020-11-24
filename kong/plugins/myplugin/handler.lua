4a38c1aa80ca40adc3c34b7df39b4e47118cd429
Joshua Schmid <jschmid@suse.de>
2020-11-24 09:17:13 +0100

    handler: change handler logic to requirements
    Signed-off-by: Joshua Schmid <jschmid@suse.de>

--- kong/plugins/myplugin/handler.lua
+++ kong/plugins/myplugin/handler.lua
@@ -4,7 +4,7 @@ local plugin = {
 }
 
 
-function requestHeaderContains(set, key)
+local function requestHeaderContains(set, key)
   kong.log.notice("Checking if request headers contain: ", key)
   -- Headers are in format HeaderName:HeaderValue
   -- This splits it in order to compare it with the request header table
@@ -26,7 +26,7 @@ function requestHeaderContains(set, key)
   return false
 end
 
-function proxy_request_to(upstream)
+local function proxy_request_to(upstream)
   -- function to abstract error handling and logging for `set_upstream`
   local ok, err = kong.service.set_upstream(upstream)
   if not ok then
@@ -37,7 +37,7 @@ function proxy_request_to(upstream)
   return
 end
 
-function find_default_target(conf)
+local function find_default_target(conf)
   -- Function to find the default target(upstream)
   -- in the plugin's config. The config validates
   -- that at least one default_target exists
@@ -51,29 +51,43 @@ function find_default_target(conf)
   end
 end
 
+local function allFiltersApply(headers, filter_rules)
+  -- Wrapper function to return if all filters for a certain rule
+  -- have applied  
+  for _, filter_header in pairs(filter_rules) do
+    if not requestHeaderContains(headers, filter_header) then
+      -- if any of the headers that are required isn't in the request headers
+      -- proxy to default upstream
+      kong.log.notice('Could not find header -> ', filter_header, " in request headers. Proxying to default upstream")
+      return false
+    end
+    -- if all items that are in _header_match_ are also in `headers`
+    -- set the upstream of data.upstream
+  end
+  return true
+end
+
+
 -- runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
 
+  -- Q: Not sure if this is necessary, there is probably a way to proxy to the configured
+  -- `host` for this route which is `europe_cluster` in this case.
   local default_target = find_default_target(plugin_conf)
 
   -- get all headers from current request
   local headers = kong.request.get_headers()
 
+  -- iterate over defined filter rules
   for _, data in ipairs(plugin_conf.filter_rules) do
     kong.log.notice("Checking filter_rules for upstream -> ", data.upstream)
-    -- iterating through header_match table
-    for _, filter_header in pairs(data.header_match) do
-      if not requestHeaderContains(headers, filter_header) then
-        -- if any of the headers that are required isn't in the request headers
-        -- proxy to default upstream
-        kong.log.notice('Could not find header -> ', filter_header, " in request headers. Proxying to default upstream")
-        return proxy_request_to(default_target)
-      end
+    if allFiltersApply(headers, data.header_match) then
+      -- if filter rules fully applies, route to respective upstream 
+      return proxy_request_to(data.upstream)
     end
-      -- if all items that are in _header_match_ are also in `headers`
-      -- set the upstream of data.upstream
-    return proxy_request_to(data.upstream)
-    end
+  end
+  -- if none of the filters fully applied, fall back to the default_target
+  return proxy_request_to(default_target)
 end 
 
 
